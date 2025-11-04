5d4b643d2f82a783c264cd7998ff1c8ca5dda16e
SRI212950 <sriram3655@gmail.com>
2025-11-04 22:38:58 +0530

    Update SrHarvesterApplication.java

--- harvester-cal/src/main/java/com/srharvester/SrHarvesterApplication.java
+++ harvester-cal/src/main/java/com/srharvester/SrHarvesterApplication.java
@@ -2,10 +2,18 @@ package com.srharvester;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SrHarvesterApplication {
+public class HarvesterCalculatorApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HarvesterCalculatorApplication.class);
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(SrHarvesterApplication.class, args);
+        SpringApplication.run(HarvesterCalculatorApplication.class, args);
     }
 }
